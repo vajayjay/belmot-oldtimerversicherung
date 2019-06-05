641b41efd8ce06a91324399380a5161799e734f1
Jannis <jannis@pixel-pink.de>
2019-06-05 19:01:38 +0200

    star rating customer form

--- src/components/CustomerRatingForm.js
+++ src/components/CustomerRatingForm.js
@@ -1,28 +1,138 @@
 import React from "react"
-import "../theme/forms.css"
+// import "../theme/forms.css"
+import { ICONS } from "../theme/Icons"
+import Icon from "../components/layout/Icon"
 
-// import styled from "styled-components"
-// import Icon from "../components/layout/Icon"
-// import { ICONS } from "../theme/Icons"
+import styled from "styled-components"
+
 // import Button from "../components/Button"
+
+const StarRating = styled.fieldset`
+    /* display: flex; */
+    border: none;
+    & > label {
+        display: inline;
+    }
+`
+const IconOff = styled.div`
+    display: inline-block;
+    vertical-align: middle;
+    & > svg {
+        fill: var(--color-medium);
+    }
+`
+
+const IconOn = styled.div`
+    display: inline-block;
+    vertical-align: middle;
+    & > svg {
+        fill: var(--color-primary);
+    }
+`
+
+const RatingRadio = styled.input`
+    display: none;
+
+    /* Hide off-icon */
+    + label ${IconOff} {
+        display: none;
+    }
+    /* Hide on-icon */
+    :checked ~ label ${IconOn} {
+        display: none;
+    }
+    :checked ~ label ${IconOff} {
+        display: inline-block;
+    }
+`
 
 const CustomerRatingForm = props => (
     <div>
-        <p>ttest</p>
+        <h2>Vielen Dank für Ihr Vertrauen.</h2>
+        <p>Wir freuen uns über eine positive Bewertung von Ihnen.</p>
         <form
             method="POST"
             action="https://dev.staticman.net/v3/entry/github/vajayjay/belmot-oldtimerversicherung/master/comments"
         >
             <label>
-                <label>
-                    <span>Ihr Name</span>
-                    <input type="text" id="form[name]" name="fields[name]" />
-                </label>
-                <label>
-                    <span>Ihre Rezension</span>
-                    <input type="text" id="form[name]" name="fields[review]" />
-                </label>
+                <span>Ihr Name</span>
+                <input type="text" id="form[name]" name="fields[name]" />
             </label>
+
+            <label>
+                <span>Ihre Rezension</span>
+                <textarea id="form[name]" name="fields[review]" />
+            </label>
+            <StarRating>
+                <span>Rating:</span>
+                <label htmlFor="customer-rating[star-1]">
+                    <IconOn>
+                        <Icon icon={ICONS.STAR} />
+                    </IconOn>
+                </label>
+                <RatingRadio
+                    type="radio"
+                    value="1"
+                    name="fields[rating]"
+                    id="customer-rating[star-1]"
+                />
+                <label htmlFor="customer-rating[star-2]">
+                    <IconOff>
+                        <Icon icon={ICONS.STAR} off />
+                    </IconOff>
+                    <IconOn>
+                        <Icon icon={ICONS.STAR} on />
+                    </IconOn>
+                </label>
+                <RatingRadio
+                    type="radio"
+                    value="2"
+                    name="fields[rating]"
+                    id="customer-rating[star-2]"
+                />
+                <label htmlFor="customer-rating[star-3]">
+                    <IconOff>
+                        <Icon icon={ICONS.STAR} off />
+                    </IconOff>
+                    <IconOn>
+                        <Icon icon={ICONS.STAR} on />
+                    </IconOn>
+                </label>
+                <RatingRadio
+                    type="radio"
+                    value="3"
+                    name="fields[rating]"
+                    id="customer-rating[star-3]"
+                />
+                <label htmlFor="customer-rating[star-4]">
+                    <IconOff>
+                        <Icon icon={ICONS.STAR} off />
+                    </IconOff>
+                    <IconOn>
+                        <Icon icon={ICONS.STAR} on />
+                    </IconOn>
+                </label>
+                <RatingRadio
+                    type="radio"
+                    value="4"
+                    name="fields[rating]"
+                    id="customer-rating[star-4]"
+                />
+                <label htmlFor="customer-rating[star-5]">
+                    <IconOff>
+                        <Icon icon={ICONS.STAR} off />
+                    </IconOff>
+                    <IconOn>
+                        <Icon icon={ICONS.STAR} on />
+                    </IconOn>
+                </label>
+                <RatingRadio
+                    type="radio"
+                    value="5"
+                    name="fields[rating]"
+                    id="customer-rating[star-5]"
+                />
+            </StarRating>
             <button>Absenden</button>
         </form>
     </div>
