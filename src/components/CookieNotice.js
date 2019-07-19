0f7116a61d73480d1c84ded8a4b381e6e344d627
Jannis <jannis@pixel-pink.de>
2019-07-19 12:04:24 +0200

    cookie banner test

--- src/components/CookieNotice.js
+++ src/components/CookieNotice.js
@@ -1,9 +1,11 @@
 import React from "react"
+// import { CookieConsent } from "cookieconsent"
 
 class CookieNotice extends React.Component {
     componentDidMount() {
         window.addEventListener("load", function() {
             window.cookieconsent.initialise({
+                container: document.getElementById("content"),
                 palette: {
                     popup: {
                         background: "#0A1468",
@@ -14,6 +16,14 @@ class CookieNotice extends React.Component {
                         text: "#ffffff",
                         border: "#fff",
                     },
+                },
+                revokable: true,
+                onStatusChange: function(status) {
+                    console.log(
+                        this.hasConsented()
+                            ? "enable cookies"
+                            : "disable cookies"
+                    )
                 },
                 position: "bottom-left",
                 content: {
--- src/components/Seo.js
+++ src/components/Seo.js
@@ -96,7 +96,6 @@ function SEO({ description, lang, meta, keywords, title }) {
                 type="text/css"
                 href="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.css"
             />
-
             <script
                 src="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.js"
                 data-cfasync="false"
