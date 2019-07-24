2a41f92531e581b053e66bd35bad6b3e8b63edd5
Jannis <jannis@pixel-pink.de>
2019-07-24 20:02:35 +0200

    fix rating form

--- src/components/CustomerRatingForm.js
+++ src/components/CustomerRatingForm.js
@@ -29,7 +29,7 @@ const IconOn = styled.div`
     & > svg {
         width: 50px;
         height: 50px;
-        fill: var(--color-primary);
+        fill: var(--color-warning);
     }
 `
 
@@ -52,26 +52,18 @@ const RatingRadio = styled.input`
 const StyledButton = styled.button`
     margin-top: var(--space-m);
     display: inline-block;
-    background: ${props =>
-        props.primary ? "var(--color-primary)" : "var(--color-white)"};
-    border: 2px solid
-        ${props =>
-            props.primary ? "var(--color-primary)" : "var(--color-white)"};
-    color: ${props =>
-        props.primary ? "var(--color-white)" : "var(--color-black)"};
-
+    background: var(--color-primary);
+    border: 2px solid var(--color-primary);
+    color: var(--color-white);
     font-size: 16px;
     font-weight: 500;
     text-align: center;
     text-transform: uppercase;
     text-decoration: none;
-
     user-select: none;
     cursor: pointer;
-
     padding: 1rem 2rem;
     margin-bottom: var(--space-s);
-
     width: 100%;
     transform: translateY(0);
     transition: all var(--time-fast);
@@ -81,16 +73,8 @@ const StyledButton = styled.button`
     }
 
     :hover {
-        color: ${props =>
-            props.primary ? "var(--color-white)" : "var(--color-black)"};
-        background: ${props =>
-            props.primary
-                ? "var(--color-primary-light)"
-                : "var(--color-lighter)"};
-        border-color: ${props =>
-            props.primary
-                ? "var(--color-primary-light)"
-                : "var(--color-lighter)"};
+        background: var(--color-primary-light);
+        border-color: var(--color-primary-light);
         transform: translateY(-1px);
         transition: all var(--time-fast);
     }
