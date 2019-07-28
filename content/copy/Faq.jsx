f92fb73b5b9b63cdf14acfab637a761b26e84b2e
Jannis <jannis@pixel-pink.de>
2019-07-28 15:28:17 +0200

    faq fixes

--- content/copy/Faq.jsx
+++ content/copy/Faq.jsx
@@ -10,15 +10,15 @@ const FAQ = [
     },
     {
         question: "Was versteht man unter dem Begriff Marktwert?",
-        answer: `Rund 75% der deutschen Oldtimerpolicen legen den sogenannten Marktwert zugrunde. Bei seltener gehandelten Fahrzeugmodellen und bei Fahrzeugen, die schwerpunktmäßig gewerblich vertrieben werden, fließen auch der Handel (als Nettobetrag), die internationalen Auktionsergebnisse (ohne MwSt.) sowie die internationale Marktsituation mit in den Marktwert ein.`,
+        answer: `Rund 75% der deutschen Oldtimerversicherungen legen als Versicherungssumme den sogenannten Marktwert zugrunde. Bei seltener gehandelten Fahrzeugmodellen und bei Fahrzeugen, die schwerpunktmäßig gewerblich vertrieben werden fließen auch die Händlergewinnspanne, die internationalen Auktionsergebnisse (ohne Mehrwertsteuer) sowie die internationale Marktsituation mit in den Marktwert ein.`,
     },
     {
         question: "Was ist der Wiederbeschaffungswert?",
-        answer: `Im Gegensatz zum Marktwert geht der Wiederbeschaffungswert von einer „kurzfristigen Wiederbeschaffung“ aus. Er wird definiert aus dem Betrag, der tatsächlich für den Kauf eines gleichartigen und gleichwertigen Ersatzfahrzeugs aufgewendet werden muss. Somit berücksichtigt er den Fahrzeugkauf bei einem Händler.`,
+        answer: `Im Gegensatz zum Marktwert geht der Wiederbeschaffungswert von einer „kurzfristigen Wiederbeschaffung“ aus und ist deutlich höher. Er wird definiert als der Betrag, der tatsächlich für den Kauf eines gleichartigen und gleichwertigen Ersatzfahrzeugs auf- gewendet werden muss. Somit berücksichtigt er den Fahrzeug- kauf bei einem Händler.`,
     },
     {
         question: "Was versteht man unter Wiederaufbauwert?",
-        answer: `Berücksichtigt die Tatsache, dass die Kosten für ein restauriertes Fahrzeug weitaus höher sind als die zu erzielenden durchschnittlichen Marktwerte. Es fließen die Restaurations- sowie die Anschaffungskosten eines restaurierungsfähigen Basisfahrzeugs mit ein. Es handelt sich um einen hypothetischen Wert für ein Fahrzeug in Originalzustand.`,
+        answer: `Der Wiederaufbauwert berücksichtigt die Tatsache, dass die Kosten für ein zu restaurierendes Fahrzeug weitaus höher sind als der durchschnittliche Marktwert. Es fließen die Restaurierungs- sowie die Anschaffungskosten eines restaurierungsfähigen Basisfahrzeugs mit ein. Es handelt sich um einen hypothetischen Wert für ein Fahrzeug in Originalzustand.`,
     },
     {
         question: "Was sind die Wiederherstellungskosten?",
@@ -47,55 +47,75 @@ const FAQ = [
     },
     {
         question: "Welcher Wert ist der richtige?",
-        answer: `Achten Sie bei Ihrer Oldtimer-Versicherung unbedingt auf eine Absicherung zum Wiederbeschaffungswert! Im Gegensatz zum Marktwert berücksichtigt dieser die kurzfristige Wiederbeschaffung eines gleichwertigen Fahrzeugs inkl. Händlergewinnspanne und Mehrwertsteuer.`,
+        answer: `Achten Sie bei Ihrer Oldtimerversicherung unbedingt auf eine Absicherung zum Wiederbeschaffungswert! Im Gegensatz zum Marktwert berücksichtigt dieser die kurzfristige Wiederbeschaffung eines gleichwertigen Fahrzeugs inklusive Händlergewinnspanne und Mehrwertsteuer.`,
     },
     {
         question: "Wann macht ein rotes Wechselkennzeichen Sinn?",
-        answer: `Das Wechselkennzeichen für mehrere Fahrzeuge gilt ausschließlich für die Teilnahme an Oldtimer-Veranstaltungen, An- und Abfahrten hierzu sowie Probe-, Prüfungs- und Überführungsfahrten und Fahrten zum Zwecke der Wartung oder Reparatur. Es gelten dieselben Bedingungen wie beim H-Kennzeichen sowie darüber hinaus die Pflicht zur Führung eines Fahrtenbuchs. Das Befahren von innerstädtischen Umweltzonen ist grundsätzlich gestattet. Alle Benziner mit Erstzulassung vor dem 01.07.1969 sowie Dieselfahrzeuge vor dem 01.07.1976 sind ohnehin von der Pflicht zur Abgasuntersuchung befreit. Das Kennzeichen besteht aus einer Kombination der Buchstabenkombination der Region sowie einer fünfstelligen Nummer, beginnend mit der Ziffernkombination '07'.
+        answer: `Das Wechselkennzeichen für mehrere Fahrzeuge gilt ausschließlich für die Teilnahme an Oldtimer-Veranstaltungen, An- und Abfahrten hierzu sowie Probe-, Prüfungs- und Überführungsfahr- ten und Fahrten zum Zwecke der Wartung oder Reparatur.
+        Es gelten dieselben Bedingungen wie beim H-Kennzeichen.
+
         
-        <h3>Kfz-Steuer</h3>Pro Kennzeichen wird (unabhängig von Hubraum und Leistung) eine Pauschalsteuer von Euro 191,73 fällig.
+        <h3>Kfz-Steuer</h3>Pro Kennzeichen wird (unabhängig von Hubraum und Leistung) eine Pauschalsteuer von zurzeit 191,73 Euro fällig.
         
-        <h3>Voraussetzungen</h3>Ihr Oldtimer muss vor mindestens 30 Jahren erstmals in den Verkehr gekommen sein und nach § 9 (1) der seit 1.3.2007 geltenden Fahrzeug-Zulassungsverordnung von einem amtlich anerkannten Sachverständigen, Prüfer oder Prüfingenieur (z.B. bei DEKRA, GTÜ, KÜS oder TÜV) ein Gutachten für die Einstufung eines Fahrzeugs als Oldtimer vorweisen können. Dabei wird festgestellt, ob sich das Fahrzeug weitgehend im Originalzustand befindet. Ein fremder Motor, eine moderne HiFi-Anlage, Sportsitze oder ein schlechter Erhaltungszustand können die Erteilung verhindern. Das Gutachten belegt, dass Ihr Fahrzeug der „Pflege des Kfz-technischen Kulturgutes“ zugeordnet werden kann. Der Gesamteindruck wird sozusagen mitbewertet.
-        
-        <h3>An- und Abmeldung</h3>Die Wechselkennzeichen sind unter Vorlage von Antragsdokumenten bei der Zulassungsbehörde erhältlich. Nutzen Sie unsere Checkliste für den besseren Überblick. Generell ist der Zulassungsbehörde ein polizeiliches Führungszeugnis vorzulegen und ein Auszug aus dem Punkteregister in Flensburg erforderlich.`,
+        <h3>An- und Abmeldung</h3>Die Wechselkennzeichen sind unter Vorlage von Antragsdokumenten bei der Zulassungsbehörde erhältlich. Generell ist der Zulassungsbehörde ein polizeiliches Führungszeugnis vor- zulegen und ein Auszug aus dem Punkteregister in Flensburg ist erforderlich.`,
     },
     {
         question: "Wann macht ein Saison-Kennzeichen Sinn?",
-        answer: `Ihr Vorteil: Sie zahlen nur Steuern für den angemeldeten Zeitraum. „Echte“ Oldtimer-Liebhaber wählen häufig die Kombination 3/10, also März bis Oktober, damit sie schon mit der ersten Märzsonne raus auf die Straßen dürfen!
-        
-        Für alle, die ihr Fahrzeug nur für begrenzte Monate des Jahres bewegen möchten! Es gelten die Bedingungen der regulären Zulassung mit dem schwarzen Kennzeichen. Das Alter des Oldtimers spielt keine Rolle. Für die Zulassung brauchen Sie lediglich eine aktuelle Versicherungsbestätigung. Ein Saisonzeitraum gilt immer vom ersten Tag 0.00 Uhr bis zum letzten Tag 24.00 Uhr. Ein Saisonkennzeichen 03-10 ist also immer vom 01. März bis zum 31. Oktober eines jeden Jahres gültig. Ihr Fahrzeug darf außerhalb des Saisonzeitraumes nicht am öffentlichen Straßenverkehr teilnehmen. Nicht zulässig ist auch das Abstellen des Fahrzeuges auf öffentlichen Straßen oder teilöffentlichem Gelände (z.B. frei zugänglicher Parkplatz).
+        answer: `Für alle, die ihr Fahrzeug nur für begrenzte Monate des Jahres bewegen möchten! Es gelten die Bedingungen der regulären Zulassung mit dem schwarzen Kennzeichen. Das Alter des Oldtimers spielt keine Rolle. Für die Zulassung brauchen Sie lediglich eine aktuelle Versicherungsbestätigung. Ein Saisonzeitraum gilt immer vom ersten Tag 0.00 Uhr bis zum letzten Tag 24.00 Uhr. Ein Saisonkennzeichen 03–10 ist also vom 1. März bis zum 31. Oktober eines jeden Jahres gültig. Ihr Fahrzeug darf außer- halb des Saisonzeitraumes nicht am öffentlichen Straßenverkehr teilnehmen. Nicht zulässig ist auch das Abstellen des Fahrzeuges auf öffentlichen Straßen oder teilöffentlichem Gelände (z.B. frei zugänglicher Parkplatz).
             
             <h3>Kfz-Steuer</h3>Die Kfz-Steuer wird anteilig nur für den Saisonzeitraum erhoben.
             
-            <h3>An- und Abmeldung</h3>Die An- und Abmeldung entfällt: Sie müssen nicht jedes Jahr aufs Neue zur Zulassungsstelle gehen, die Zulassung tritt automatisch wieder in Kraft. Es fallen auch nur einmalig – wie beim schwarzen Kennzeichen – die Gebühren zur Zulassung an.`,
+            <h3>An- und Abmeldung</h3>Die An- und Abmeldung entfällt: Sie müssen nicht jedes Jahr aufs Neue zur Zulassungsstelle gehen, die Zulassung tritt automatisch wieder in Kraft. Es fallen auch nur einmal – wie beim schwarzen Kennzeichen – die Gebühren für die Zulassung an.`,
     },
     {
         question: "Wann macht ein schwarzes Kennzeichen Sinn?",
-        answer: `Eher etwas für Youngtimer: Die Standardzulassung ist für einen Oldtimer nicht empfehlenswert, da sie Ihr Fahrzeug nach Hubraum besteuert und dadurch oftmals teurer ist als das H-Kennzeichen. Auch die Fahrt in Umweltzonen ist oftmals nicht möglich.
-        
-        Ob mit oder ohne Euro-Symbol: Das schwarze Kennzeichen steht für die reguläre Zulassung nach den Kriterien der Straßenverkehrs-Zulassungsordnung. Das Alter des Fahrzeugs spielt hierbei keine Rolle. Es wird lediglich eine aktuelle Versicherungsbestätigung benötigt. Achtung Feinstaubzonen: Trägt Ihr Oldtimer keine Feinstaubplakette, ist das Befahren von innerstädtischen Umweltzonen mit dem schwarzen Kennzeichen nicht gestattet.
-        Tipp: Ein H-Kennzeichen darf überall fahren.
+        answer: `Eher etwas für Youngtimer: Die Standardzulassung ist für einen Oldtimer nicht empfehlenswert, da sie das Fahrzeug nach Hub- raum besteuert und dadurch oftmals teurer ist als das H-Kennzeichen. Auch die Fahrt in Umweltzonen ist oftmals nicht möglich.
             
-        <h3>Kfz-Steuer</h3>Für den Besitzer eines Oldtimers bietet das schwarze Kennzeichen nur unter bestimmten Voraussetzungen Vorteile. Durch die konventionelle Hubraumbesteuerung ist es in der Regel teurer als ein historisches Kennzeichen. Folgende Steuersätze sind seit dem 01.04.2007 gültig (gilt für Fahrzeuge, die schlechter eingestuft sind als Euro 1):
-            Benziner: 25,36 Euro je angefangene 100 ccm Hubraum\nDiesel: 38,78 Euro je angefangene 100 ccm Hubraum
-            
-            Zum Vergleich: Ein Oldtimer mit H-Kennzeichen kostet Sie nur EUR 191,- p.a., unabhängig vom Hubraum.
+        <h3>Kfz-Steuer</h3>Für den Besitzer eines Oldtimers bietet das schwarze Kennzeichen nur unter bestimmten Voraussetzungen Vorteile. Durch die konventionelle Hubraumbesteuerung ist es in der Regel teurer als ein historisches Kennzeichen. Folgende Steuersätze sind seit April 2007 gültig (gilt für Fahrzeuge, die schlechter eingestuft sind als Euro 1):
+        Benziner: 25,36 Euro je angefangene 100 cm<sup>3</sup> Hubraum Diesel: 38,78 Euro je angefangene 100 cm<sup>3</sup> Hubraum.
+        Zum Vergleich: Ein Oldtimer mit H-Kennzeichen kostet Sie zurzeit nur 191,73 Euro p. a., unabhängig vom Hubraum.
 
-            <h3>An- und Abmeldung</h3>Achten Sie – je nach Landkreis – bei einer vorübergehenden Stilllegung, z. B. zwecks Restauration oder Winterruhe, auf die Reservierung Ihres Kennzeichens bis zum Zeitpunkt der Wiederzulassung. Die Kfz-Steuer wird taggenau abgerechnet.
-
-            <h3>Wiederzulassung</h3>Wichtig: Ihr Fahrzeug benötigt eine gültige TÜV-Untersuchung.\nBesitzt das Fahrzeug aufgrund längerer Stilllegung noch einen alten Fahrzeugbrief? Dann ist zu prüfen, ob die Abmeldung bereits länger als 18 Monate zurückliegt. Falls ja, ist die Betriebserlaubnis erloschen und eine Vollabnahme des TÜV erforderlich, z. B. nach § 23 (Oldtimergutachten). Auf jeden Fall erhalten Sie die seit 01.03.2007 gültigen neuen Fahrzeugpapiere. Ihren alten Fahrzeugbrief sollten Sie sich auf jeden Fall aushändigen lassen. Aus diesem geht die Besitzhistorie hervor und oftmals sind Bauteile oder Ausstattungen eingetragen, die aus dem neuen Dokument nicht hervorgehen. Besitzt Ihr Fahrzeug schon die aktuellen EU-Papiere, dann benötigen Sie kein Vollgutachten, falls Ihr Fahrzeug nicht länger als 7 Jahre stillgelegt war.`,
+        <h3>An- und Abmeldung</h3>Achten Sie – je nach Landkreis – bei einer vorübergehenden Stilllegung, z. B. zwecks Restaurierung oder Winterruhe, auf die Reservierung Ihres Kennzeichens bis zum Zeitpunkt der Wiederzulassung. Die Kfz-Steuer wird taggenau abgerechnet.`,
     },
     {
         question: "Wann macht ein H-Kennzeichen Sinn?",
-        answer: `H wie historisch: Ihr Oldtimer muss hierfür mindestens 30 Jahre alt sein und per Gutachten nachweisen, dass er der „Pflege des Kfz-technischen Kulturgutes“ zugeordnet werden kann. Das bedeutet, dass er im (gut erhaltenen) Originalzustand sein muss. Außerdem: Freie Fahrt in Feinstaubzonen!
-            Alle Benziner mit Erstzulassung vor dem 01.07.1969 sowie Dieselfahrzeuge vor dem 01.07.1976 sind ohnehin von der Pflicht zur Abgasuntersuchung befreit. Das H-Kennzeichen wird in den Papieren mit der Schlüsselnummer „098“ vermerkt.
+        answer: `H wie historisch: Ihr Oldtimer muss hierfür mindestens 30 Jahre alt sein und Sie müssen per Gutachten nachweisen, dass er der „Pflege des Kfz-technischen Kulturgutes“ zugeordnet werden kann. Das bedeutet, dass er in gut erhaltenem Originalzustand sein muss. Übrigens: Fahrzeuge mit H-Kennzeichen genießen freie Fahrt in Feinstaubzonen.
 
-            <h3>Kfz-Steuer</h3>Der Steuersatz wird pauschal erhoben:
-            EUR 191,73 p.a. für Kraftfahrzeuge\nEUR 46,00 p.a. für Krafträder
-            
-            <h3>Voraussetzungen</h3>Ihr Oldtimer muss vor mindestens 30 Jahren erstmals in den Verkehr gekommen sein und nach § 9 (1) der seit 1.3.2007 geltenden Fahrzeug-Zulassungsverordnung von einem amtlich anerkannten Sachverständigen, Prüfer oder Prüfingenieur (z.B. bei DEKRA, GTÜ, KÜS oder TÜV) ein Gutachten für die Einstufung eines Fahrzeugs als Oldtimer vorweisen können. Dabei wird festgestellt, ob sich das Fahrzeug weitgehend im Originalzustand befindet. Ein fremder Motor, eine moderne HiFi-Anlage, Sportsitze oder ein schlechter Erhaltungszustand können die Erteilung verhindern. Das Gutachten belegt, dass Ihr Fahrzeug der „Pflege des Kfz-technischen Kulturgutes“ zugeordnet werden kann. Der Gesamteindruck wird sozusagen mitbewertet.
-            
-            <h3>An- und Abmeldung</h3>Wie bei einem regulären schwarzen Kennzeichen kann die Dauer der Zulassung frei gewählt werden. Die Kfz-Steuer wird taggenau abgerechnet. Viele Oldtimer-Fahrer verzichten aufgrund des günstigen Steuersatzes auf die Abmeldung über Winter.`,
+        <h3>Kfz-Steuer</h3>Der Steuersatz wird pauschal erhoben: 191,73 Euro p. a. für Kraftfahrzeuge. 46 Euro p. a. für Krafträder.
+        
+        <h3>An- und Abmeldung</h3>Wie bei einem regulären schwarzen Kennzeichen kann die Dauer der Zulassung frei gewählt werden. Die Kfz-Steuer wird taggenau abgerechnet. Viele Oldtimerfahrer verzichten aufgrund des günstigen Steuersatzes auf die Abmeldung über den Winter.`,
+    },
+    {
+        question: "Wie wähle ich den richtigen Abstellort?",
+        answer: `<h4>Tiefgarage</h4>Nicht empfehlenswert für hochwertige Klassiker. Besonders Parkhäuser und öffentliche Tiefgaragenparkplätze sind als dauerhafter Stellplatz ungeeignet – auch im Hinblick auf den Werterhalt Ihres Fahrzeugs.
+
+        <h4>Carport</h4>Einfachste Möglichkeit, ein Fahrzeug trocken unter zustellen.
+        Der Carport sollte sich auf einem um zäunten Grundstück befinden
+        und nicht von außen einsehbar sein. Für hochwertige Klassiker
+        nicht zu empfehlen.
+
+        <h4>Halle</h4>Ein beliebter Ort, um Sammlungen abzustellen. Die Halle sollte in
+        einem guten baulichen Zustand und durch besondere Maßnahmen
+        gegen Einbruch gesichert sein. Damit die Fahrzeuge vor Rost und
+        Schimmel bewahrt werden, empfiehlt sich eine Be- und Entlüftungsanlage einzubauen.
+        
+        <h4>Einzelgarage</h4>Einzel- und Doppelgaragen sind optimale Abstellorte. Aber auch hier
+        sind Einbruchmöglichkeiten gegeben. Eine Einbruchmeldeanlage
+        schafft Abhilfe. Aufgrund der Luftzirkulation sollte die Anschaffung
+        einer Be- und Entlüftungsanlage in Erwägung gezogen werden.
+        Besonders zu empfehlen bei hochwertigen Oldtimern.`,
+    },
+    {
+        question: "Welche Sicherungsmaßnahmen kann ich treffen?",
+        answer: `<h4>Mechanisch sichern</h4>Sie können Gaunern das Leben erschweren, indem Sie Ihren Oldtimer mechanisch sichern.  Vor allem unterwegs bei einer Ausfahrt oder einem Kurztrip verhindern Sicherungen den Gelegenheitsdiebstahl.
+        
+        In Frage kommen
+        - eine Lenkrad- und/oder Pedalkralle,
+        - eine mechanische oder elektronische Wegfahrsperre,
+        - eine Parkkralle.
+
+        <h4>Elektronisch sichern</h4>Mit dem Einbau eines Ortungsmoduls erhöhen Sie die Chance auf das Wiederauffinden Ihres Fahrzeuges nach einem Diebstahl deutlich.
+`,
     },
     {
         question: "Wie melde ich einen Schaden?",
