ea13c7d0df1c8637d700297307b605b398cf6946
Jannis <jannis@pixel-pink.de>
2019-07-28 16:13:11 +0200

    removed css plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,13 +16,6 @@ module.exports = {
             },
         },
         `gatsby-plugin-postcss`,
-        {
-            resolve: `gatsby-plugin-css-customs`,
-            // defaults
-            options: {
-                cssModules: false,
-            },
-        },
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
         {
