8883835583a154f8cb13f6cbb2f3d3e948bf0baa
Jannis <jannis@pixel-pink.de>
2020-03-12 20:35:50 +0100

    added support for anchor links

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,31 @@
  */
 
 // You can delete this file if you're not using it
+
+exports.onRouteUpdate = ({ location }) => {
+    anchorScroll(location)
+    return true
+}
+exports.shouldUpdateScroll = ({
+    routerProps: { location },
+    getSavedScrollPosition,
+}) => {
+    anchorScroll(location)
+    return true
+}
+
+function anchorScroll(location) {
+    // Check for location so build does not fail
+    if (location && location.hash) {
+        setTimeout(() => {
+            // document.querySelector(`${location.hash}`).scrollIntoView({ behavior: 'smooth', block: 'start' });
+            const item = document.querySelector(`${location.hash}`).offsetTop
+            const mainNavHeight = document.querySelector(`nav`).offsetHeight
+            window.scrollTo({
+                top: item - mainNavHeight,
+                left: 0,
+                behavior: "smooth",
+            })
+        }, 0)
+    }
+}
